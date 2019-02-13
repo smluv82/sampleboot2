006ff1ce7ee1183de241471b3a2a35dcc9e038cc
smluv82 <smluv82@naver.com>
2019-02-13 14:50:03 +0900

    ss

---
+++ src/main/java/com/sample/demo/config/CacheConfiguration.java
@@ -0,0 +1,56 @@
+package com.sample.demo.config;
+
+import javax.annotation.Resource;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.sample.demo.common.redis.JsonRedisSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+	@Resource
+	private JedisConnectionFactory jedisConnectionFactory;
+
+	@Bean
+	public JsonRedisSerializer jsonRedisSerializer() {
+		return new JsonRedisSerializer();
+	}
+
+	private RedisCacheConfiguration redisCacheConfiguration() {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+				.disableCachingNullValues()
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer()))
+				;
+		redisCacheConfiguration.usePrefix();
+
+		return redisCacheConfiguration;
+	}
+	/**
+	 * redis cache manager
+	 *
+	 * @return
+	 */
+	@Bean
+	public CacheManager redisCacheManager() {
+//		RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate());
+//		redisCacheManager.setUsePrefix(true);
+
+		return RedisCacheManager
+				.builder(jedisConnectionFactory)
+				.cacheDefaults(redisCacheConfiguration())
+				.build();
+	}
+}
--- src/main/java/com/sample/demo/config/RedisDataConfiguration.java
+++ src/main/java/com/sample/demo/config/RedisDataConfiguration.java
@@ -1,15 +1,12 @@
 package com.sample.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.sample.demo.common.redis.JsonRedisSerializer;
@@ -17,7 +14,7 @@ import com.sample.demo.common.redis.JsonRedisSerializer;
 @Configuration
 public class RedisDataConfiguration {
 
-	@Autowired
+	@Resource
 	private JedisConnectionFactory jedisConnectionFactory;
 
 	@Bean
@@ -43,28 +40,5 @@ public class RedisDataConfiguration {
 		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(jedisConnectionFactory);
 		return redisTemplate;
-	}
-
-	// cache bean 선언.
-	@Bean
-	public CacheManager redisCacheManager() {
-//		RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate());
-//		redisCacheManager.setUsePrefix(true);
-
-		return RedisCacheManager
-				.builder(jedisConnectionFactory)
-				.cacheDefaults(redisCacheConfiguration())
-				.build();
-	}
-
-	private RedisCacheConfiguration redisCacheConfiguration() {
-		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-				.disableCachingNullValues()
-				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer()))
-				;
-		redisCacheConfiguration.usePrefix();
-
-		return redisCacheConfiguration;
 	}
 }
