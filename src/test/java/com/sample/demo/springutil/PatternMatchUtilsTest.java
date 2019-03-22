b9f563505cea44ceb18c1cdff4b6e21d214d0312
smluv82 <smluv82@naver.com>
2019-03-22 11:34:30 +0900

    내장WAS 톰캣에서 undertow로 변경

---
+++ src/test/java/com/sample/demo/springutil/PatternMatchUtilsTest.java
@@ -0,0 +1,14 @@
+package com.sample.demo.springutil;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatternMatchUtilsTest {
+
+}
