62651ce9900a558b89d775db38d8fdaeb18d41a3
smluv82 <smluv82@naver.com>
2019-04-08 11:38:32 +0900

    소스 수정

--- src/main/java/com/sample/demo/biz/oauth/service/OauthMemberService.java
+++ src/main/java/com/sample/demo/biz/oauth/service/OauthMemberService.java
@@ -31,16 +31,43 @@ public class OauthMemberService {
 //		return oauthMemberRepository.findByEmail(email);
 //	}
 
+//	@HystrixCommand(
+//			//히스트릭스를 사용자 정의하기 위해 추가 매개변수 전달
+//			commandProperties= {
+//					@HystrixProperty(
+//							//회로 차단기의 타임아웃 시간(단위 : 미리세컨)을 설정하는데 사용 프로퍼티
+//							name="execution.isolation.thread.timeoutInMilliseconds", value="3000"
+//					)
+//			},
+//			//fallbackMethod 속성으로 히스트릭스에서 호출이 실패할 때 불러오는 클래스 메서드
+//			fallbackMethod="findByFallback"
+//			)
+//	public OauthMember findByEmail(final String email) throws Exception {
+//		log.info("findByEmail : {}", email);
+//		testHystrix();
+//		return oauthMemberRepository.findByEmail(email);
+//	}
+
 	@HystrixCommand(
 			//히스트릭스를 사용자 정의하기 위해 추가 매개변수 전달
 			commandProperties= {
 					@HystrixProperty(
 							//회로 차단기의 타임아웃 시간(단위 : 미리세컨)을 설정하는데 사용 프로퍼티
 							name="execution.isolation.thread.timeoutInMilliseconds", value="3000"
-					)
+							)
 			},
 			//fallbackMethod 속성으로 히스트릭스에서 호출이 실패할 때 불러오는 클래스 메서드
-			fallbackMethod="findByFallback"
+			fallbackMethod="findByFallback",
+			//threadPoolKey 속성은 스레드 풀의 고유 이름 지정
+			threadPoolKey="findByThreadPool",
+			//threadPoolProperties 속성은 threadPool 동작을 정의하고 설정
+			threadPoolProperties= {
+					//coreSize 속성은 스레드 풀의 개수 정의
+					@HystrixProperty(name="coreSize", value="30"),
+					//스레드가 전부 job 수행 시 maxQueueSize는 스레드 풀 앞에 배치 할 큐와 큐에 넣을 요청 수 정의
+					//maxQueueSize가 -1이면 SynchronousQueue 사용, 1 이상이면 LinkedBlockingQueue를 사용
+					@HystrixProperty(name="maxQueueSize", value="10")
+			}
 			)
 	public OauthMember findByEmail(final String email) throws Exception {
 		log.info("findByEmail : {}", email);
